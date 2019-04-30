c8e780952abaa2822fb397e63022a863c8d040f0
Matt Weiss <weiss.matt@outlook.com>
2019-04-29 23:36:28 -0600

    Adds search controller and finders

---
+++ app/controllers/api/v1/merchants/search_controller.rb
@@ -0,0 +1,22 @@
+class Api::V1::Merchants::SearchController < ApplicationController
+
+  def index
+    # binding.pry
+    render json: Merchant.where(search_params)
+  end
+
+  def show
+    render json: Merchant.find_by(search_params)
+  end
+
+  private
+
+  def search_params
+    attributes = ["name", "id", "created_at", "updated_at"]
+    attributes.each do |attribute|
+      if params.keys.include?(attribute)
+        return search_params = {attribute => params[attribute]}
+      end
+    end
+  end
+end
