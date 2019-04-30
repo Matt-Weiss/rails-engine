8db788480cd2abf80546ee578dd4af3718d17f20
Matt Weiss <weiss.matt@outlook.com>
2019-04-30 15:53:21 -0600

    Adds customers

--- app/serializers/merchant_search_serializer.rb
+++
@@ -1,4 +0,0 @@
-class MerchantSearchSerializer
-  include FastJsonapi::ObjectSerializer
-  attributes :id, :name, :created_at, :updated_at
-end
---
+++ spec/requests/api/v1/customers/customers_finder_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+
+describe "Customers API finders" do
+  it "can search by id" do
+    create_list(:customer, 3)
+    expected_id = Customer.first.id
+
+    get "/api/v1/customers/find?id=#{expected_id}"
+
+    customer = JSON.parse(response.body)
+
+    expect(response).to be_successful
+    expect(customer["data"]["id"]).to eq(expected_id.to_s)
+  end
+
+  it "can search by first name" do
+    create_list(:customer, 3)
+    expected_first_name = Customer.first.first_name
+    get "/api/v1/customers/find?first_name=#{expected_first_name}"
+
+    customer = JSON.parse(response.body)
+
+    expect(response).to be_successful
+    expect(customer["data"]["attributes"]["first_name"]).to eq(expected_first_name)
+  end
+
+  it "can search by last name" do
+    create_list(:customer, 3)
+    expected_last_name = Customer.first.last_name
+    get "/api/v1/customers/find?last_name=#{expected_last_name}"
+
+    customer = JSON.parse(response.body)
+
+    expect(response).to be_successful
+    expect(customer["data"]["attributes"]["last_name"]).to eq(expected_last_name)
+  end
+
+
+
+end
