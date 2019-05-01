6f7a4670a12963099c562d0fa9da64140e0eb94a
Matt Weiss <weiss.matt@outlook.com>
2019-04-30 20:45:04 -0600

    Adds routes for all models

--- config/routes.rb
+++ config/routes.rb
@@ -11,9 +11,13 @@ Rails.application.routes.draw do
         get '/find', to: 'search#show'
         get '/find_all', to: 'search#index'
       end
-      
+
       resources :merchants, only: [:index, :show]
       resources :customers, only: [:index, :show]
+      resources :items, only: [:index, :show]
+      resources :invoices, only: [:index, :show]
+      resources :transactions, only: [:index, :show]
+      resources :invoice_items, only: [:index, :show]
     end
   end
 end
